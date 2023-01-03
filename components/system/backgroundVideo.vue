e295ef640cc7b4d9f67652f1dca97fc4e0c3f752
Umberto Conti <dev@contiumberto.it>
2023-01-04 00:46:54 +0100

    video bkg

---
+++ components/system/backgroundVideo.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+.backgroundVideo(ref="container")
+    video(:src="props.video.filename" autoplay muted loop ref="video")
+    video.cover(:src="props.video.filename" autoplay muted loop)
+    cursor
+</template>
+
+<script setup>
+const props = defineProps(['video']);
+// const container = ref();
+// const video = ref();
+// onMounted(() => {
+//     const copy = video.value.cloneNode(true);
+//     copy.classList.add('cover');
+//     copy.setAttribute('autoplay',false);
+//     container.value.appendChild(copy);
+//     console.log(copy);
+
+// })
+
+</script>
+
+<style lang="scss">
+.backgroundVideo{
+    width: vw(100);
+    height: vh(100);
+    position: relative;
+    z-index: 1;
+    > video {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        z-index: 2;
+        clip-path: url(#cursor);
+        &.cover{
+            z-index: 1;
+            filter: grayscale(100);
+            clip-path: none;
+        }
+    }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,10 +3,7 @@
     mainHeader
     slot
     .content(v-if="content")
-        .video
-            video(:src="content.video.filename" autoplay muted loop)
-            video.cover(:src="content.video.filename" autoplay muted loop)
-            cursor
+        backgroundVideo(v-if="content.video" :video="content.video")
 </template>
 <script setup>
 const { data:story, pending, error } = await useFetch('/api/storyblok/stories/homepage');
@@ -20,27 +17,7 @@ const { content } = story.value;
         height: vh(100);
         overflow: hidden;
         > .content{
-            > .video{
-                width: vw(100);
-                height: vh(100);
-                position: relative;
-                z-index: 1;
-                > video {
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    width: 100%;
-                    height: 100%;
-                    object-fit: cover;
-                    z-index: 2;
-                    clip-path: url(#cursor);
-                    &.cover{
-                        z-index: 1;
-                        filter: grayscale(100);
-                        clip-path: none;
-                    }
-                }
-            }
+
         }
         
     }
