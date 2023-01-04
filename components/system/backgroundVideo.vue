b158825b77dd946577c0528e82ab17c8b629223b
Umberto Conti <dev@contiumberto.it>
2023-01-04 12:55:18 +0100

    background video into canvas

--- components/system/backgroundVideo.vue
+++ components/system/backgroundVideo.vue
@@ -1,22 +1,53 @@
 <template lang="pug">
-.backgroundVideo(ref="container")
-    video(:src="props.video.filename" autoplay muted loop ref="video")
-    video.cover(:src="props.video.filename" autoplay muted loop)
+.backgroundVideo
+    video(:src="props.video.filename" paused muted loop ref="video")
+    canvas(ref="canvas")
     cursor
 </template>
 
 <script setup>
 const props = defineProps(['video']);
-// const container = ref();
-// const video = ref();
-// onMounted(() => {
-//     const copy = video.value.cloneNode(true);
-//     copy.classList.add('cover');
-//     copy.setAttribute('autoplay',false);
-//     container.value.appendChild(copy);
-//     console.log(copy);
+const video = ref();
+const canvas = ref();
+onMounted(() => {
+    init();
+})
 
-// })
+const init = () => {
+    var imageToDraw = null;
+    video.value.addEventListener('play', (vid) => {
+        imageToDraw = vid.srcElement;
+
+        const loop = () => {
+            canvas.value.width = video.value.clientWidth;
+            canvas.value.height = video.value.clientHeight;
+
+            const ctx = canvas.value.getContext('2d');
+            const imageWidth = vid.srcElement.videoWidth;
+            const imageHeight = vid.srcElement.videoHeight;
+
+            drawImageScaled(imageToDraw, imageWidth, imageHeight, ctx)
+            setTimeout(loop, 1000 / 30);
+        }
+        loop();
+    })
+    video.value.play();
+}
+
+const drawImageScaled = function (img, imgWidth, imgHeight, ctx) {
+    var canvas = ctx.canvas;
+    var hRatio = canvas.width  / imgWidth;
+    var vRatio =  canvas.height / imgHeight;
+    var ratio  = Math.min ( hRatio, vRatio );
+    
+    console.log(canvas.width, imgWidth, hRatio);
+
+    var centerShift_x = ( canvas.width - imgWidth*ratio ) / 2;
+    var centerShift_y = ( canvas.height - imgHeight*ratio ) / 2;  
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.drawImage(img, 0,0, imgWidth, imgHeight, centerShift_x, centerShift_y, imgWidth*ratio, imgHeight*ratio);  
+    // ctx.drawImage(img, 0,0, imgWidth, imgHeight);  
+}
 
 </script>
 
@@ -32,14 +63,23 @@ const props = defineProps(['video']);
         left: 0;
         width: 100%;
         height: 100%;
-        object-fit: cover;
+        // object-fit: cover;
         z-index: 2;
         clip-path: url(#cursor);
-        &.cover{
-            z-index: 1;
-            filter: grayscale(100);
-            clip-path: none;
-        }
+        // &.cover{
+        //     z-index: 1;
+        //     filter: grayscale(100);
+        //     clip-path: none;
+        // }
+    }
+    > canvas{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        filter: grayscale(100);
+        opacity: .5;
     }
 }
 </style>
