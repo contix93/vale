3374f8f731ff497ca9e48bf511ba8b9baf45b644
Umberto Conti <dev@contiumberto.it>
2023-02-12 16:00:59 +0100

    modelViewer

--- components/system/modelContainer.vue
+++ components/system/modelContainer.vue
@@ -40,14 +40,25 @@ const init = () => {
 const startInterval = () => {
     intervalHolder = setInterval(() => {
         changeCameraPosition(currentIndex);
-    }, 5000)
+    }, 10000)
 }
 const changeCameraPosition = (index) => {
     if(cameraPositions && cameraPositions.length > 0){
         if(!isNaN(index)) currentIndex = index;
 
-        var orbit = cameraPositions[currentIndex].cameraY+'deg '+cameraPositions[currentIndex].cameraX+'deg '+cameraPositions[currentIndex].distance+'%';
-        modelViewer.cameraOrbit = orbit;
+        modelViewer.autoRotate = cameraPositions[currentIndex].autorotate ?? false;
+
+        if(cameraPositions[currentIndex].cameraX && cameraPositions[currentIndex].cameraY && cameraPositions[currentIndex].distance){
+            var orbit = cameraPositions[currentIndex].cameraX+'deg '+cameraPositions[currentIndex].cameraY+'deg '+cameraPositions[currentIndex].distance+'%';
+            modelViewer.cameraOrbit = orbit;
+        }
+
+        var targetX = cameraPositions[currentIndex].targetX ? cameraPositions[currentIndex].targetX+'cm' : 'auto'
+        var targetY = cameraPositions[currentIndex].targetY ? cameraPositions[currentIndex].targetY+'cm' : 'auto'
+        var targetZ = cameraPositions[currentIndex].targetZ ? cameraPositions[currentIndex].targetZ+'cm' : 'auto'
+        var target = targetX+' '+targetY+' '+targetZ;
+
+        modelViewer.cameraTarget = target;
 
         currentIndex++;
 
