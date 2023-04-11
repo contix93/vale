d120b09afae24b230f3722df54c6a95931c654f8
Umberto Conti <dev@contiumberto.it>
2023-04-11 22:19:59 +0200

    simpleLogo list

---
+++ components/body/componentsList.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.componentsList(v-if="blok && blok.items")
+    component(v-for="item in blok.items" :key="item._uid" :is="formatName(item.component)" :blok="item")
+</template>
+
+<script setup>
+const props = defineProps(['blok']);
+
+const formatName = (s) => {
+    var str = s.replace('body','');
+    return str.charAt(0).toLowerCase() + str.slice(1);
+}
+
+</script>
+<style lang="scss">
+.componentsList{
+    display: flex;
+    flex-wrap: wrap;
+    gap: mw(1.5)
+}
+</style>
---
+++ components/body/simpleLogo.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+a.simpleLogo(v-if="blok" :href="blok.url" target="_blank")
+    img2x(:data="blok.image" :fitIn="true")
+    span(v-html="blok.text")
+</template>
+
+<script setup>
+const props = defineProps(['blok']);
+</script>
+<style lang="scss">
+.simpleLogo{
+    flex: 0 0 mw(4);
+    text-align: center;
+    > .wrapper{
+        width: 100%;
+        height: auto;
+        aspect-ratio: 1 / 1;
+        margin-bottom: 1rem;
+    }
+}
+</style>
